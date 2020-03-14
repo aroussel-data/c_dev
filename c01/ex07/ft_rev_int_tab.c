9c43de7d6c1f7f7b44d72361863c35d7b629f5ad
arousselDDA <alex@deepdiveanalytics.com.au>
2020-03-14 15:56:04 +0100

    adding file needed for ex07

---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int i;
+	int temp;
+
+	i = 0;
+	temp = 0;
+	while (i < (size/2))
+	{
+		temp = tab[i];
+		tab[i] = tab[(size - 1) - i];
+		tab[(size - 1) - i] = temp;
+		i++;
+	}
+}
