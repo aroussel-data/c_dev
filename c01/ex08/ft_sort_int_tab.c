7579b8c0d1482a5aa3a4dd0e2c6b11cc811e75fc
DDAaroussel <alex@deepdiveanalytics.com.au>
2020-03-16 18:14:31 +0100

    finished testing file needed for ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,56 +1,37 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/16 10:05:12 by marvin            #+#    #+#             */
+/*   Updated: 2020/03/16 18:14:02 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 void	ft_sort_int_tab(int *tab, int size)
 {
-	// sort often involves checking value of current value and comparing with value of next value to see if it is larger or not. 
-	
-	// traverse the entirety of the array?
 	int i;
+	int t;
 	int temp;
 	
 	i = 0;
 	temp = 0;
-	
-	//printf("%d", tab[i]);
-	//printf("%d", tab[i+1]);
 
-	
-	while (tab[size-1] < tab[0])
-	{
-		// we have this [4, 3, 2, 1]
-		// we want this [1, 2, 3, 4]
-		// while value @ pos 0 > value @ pos 3 
-		// compare 4 -> 3 : is 4 > 3? If so, set pos 0 to 3 and pos 0 + 1 to 4...
-		// i++
-		// move onto i = 1
-		// is 3 > 2? If so, set pos 1 to 2 and pos 2 to 3
-		// move onto i = 2
-		// is 2 > 1? If so, set pos 2 to 1 and pos 3 to 2
-		//
-		// at this point we have something like: [3, 4, 1, 2] , so we need to do multiple passes...
-		
-		
-		if (tab[i] > tab[i+1])
+	while (i < (size-1)){
+
+		t=0;
+		while (t < (size-1))
 		{
-			//need to swap current -> next so that the smallest of the two is in the leftmost position.
-			temp = tab[i];
-			tab[i] = tab[i+1];
-			tab[i+1] = temp;	
+			if (tab[t] > tab[t+1])
+			{
+				temp = tab[t];
+				tab[t] = tab[t+1];
+				tab[t+1] = temp;
+			}
+			t = (t + 1) % size;
 		}
-		printf("%d", tab[i]);
 		i++;
-		if (i == size-1)
-		{
-			// reset i to 
-			i = 0;
-		}
 	}
-	//printf("%d", tab[i+1]);
-	
-}
-
-int	main(void)
-{
-	int int_array[4] = {4, 3, 2, 1};
-	ft_sort_int_tab(int_array, 4);
 }
