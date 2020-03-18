0008994f7c890fd18e5c0a6ed417d5291bffc638
DDAaroussel <alex@deepdiveanalytics.com.au>
2020-03-18 14:04:12 +0100

    cleaning up to align with norm

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -6,11 +6,9 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/17 21:28:17 by marvin            #+#    #+#             */
-/*   Updated: 2020/03/18 13:56:21 by marvin           ###   ########.fr       */
+/*   Updated: 2020/03/18 14:03:19 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -41,12 +39,4 @@ int	ft_strcmp(char *s1, char *s2)
 	{
 		return 1;
 	}
-}
-
-int	main(void)
-{
-	char str1[] = "abc";  //this should be greater than str2, i.e. return 1.
-	char str2[] = "ABC";
-	
-	printf("%d", ft_strcmp(str1, str2));
 }
--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,15 +6,12 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/18 13:40:27 by marvin            #+#    #+#             */
-/*   Updated: 2020/03/18 13:55:58 by marvin           ###   ########.fr       */
+/*   Updated: 2020/03/18 14:03:40 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	// like strcmp but we only compare the first n bytes of the two strings, i.e. while i < n
 	int i;
 	int s1_result;
 	int s2_result;
@@ -42,11 +39,4 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	{
 		return 1;
 	}
-}
-
-int	main(void)
-{
-	char str1[] = "abc";
-	char str2[] = "abc";
-	printf("%d\n", ft_strncmp(str1, str2, 2));
 }
