a43d47090b0dfc7baffc79510150b59ff3ae5f3e
DDAaroussel <alex@deepdiveanalytics.com.au>
2020-03-18 17:58:56 +0100

    tested functions against standard functions

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/18 14:23:11 by marvin            #+#    #+#             */
-/*   Updated: 2020/03/18 17:57:57 by marvin           ###   ########.fr       */
+/*   Created: 2020/03/18 15:53:05 by marvin            #+#    #+#             */
+/*   Updated: 2020/03/18 17:58:23 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,33 @@ int	ft_strlen(char *str)
 	return result;
 }
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strstr(char *str, char *to_find)
 {
 	int i;
-	int dest_length;
+	int j;
+	int to_find_length;
 
 	i = 0;
-	dest_length = ft_strlen(dest);
-
-	while(src[i] != '\0')
+	j = 0;
+	to_find_length = ft_strlen(to_find);
+	while (str[i] != '\0')
 	{
-		dest[dest_length + i] = src[i];
+		if (str[i] == to_find[j])
+		{
+			j++;
+			if (j == to_find_length)
+			{
+				return &str[i - (to_find_length - 1)];
+			}
+		}
 		i++;
 	}
-	dest[dest_length + i] = '\0';
-       return dest;	
+	if (*to_find == '\0')
+	{
+		return str;
+	}
+	else
+	{
+		return NULL;
+	}
 }
