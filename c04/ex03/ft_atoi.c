88a3380f57c539b626a90e414db0fe9226c0da89
DDAaroussel <alex@deepdiveanalytics.com.au>
2020-03-20 13:18:38 +0100

    cleaning up code

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,12 +6,28 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/19 19:29:52 by marvin            #+#    #+#             */
-/*   Updated: 2020/03/20 12:52:23 by marvin           ###   ########.fr       */
+/*   Updated: 2020/03/20 13:18:08 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int	result_checker(int *result, int *num_dash)
+{
+	if (*result < 0 && *num_dash % 2)
+	{
+		return *result;
+	}
+	else if (*result > 0 && *num_dash % 2)
+	{
+		return -(*result);
+	}
+	else
+	{
+		return *result;
+	}
+}
 
 int	ft_atoi(char *str)
 {
@@ -36,33 +52,11 @@ int	ft_atoi(char *str)
 
 		if (!(str[i] >= 48 && str[i] <= 57) && result != 0)
 		{
-			if (result < 0 && num_dash % 2)
-			{
-				return result;
-			}
-			else if (result > 0 && num_dash % 2)
-			{
-				return -result;
-			}
-			else
-			{
-				return result;
-			}
+			return result_checker(&result, &num_dash);
 		}
 		i++;
 	}
-	if (result < 0 && num_dash % 2)
-	{
-		return result;
-	}
-	else if (result > 0 && num_dash % 2)
-	{
-		return -result;
-	}
-	else
-	{
-		return result;
-	}
+	return result_checker(&result, &num_dash);
 }
 
 int	main(void)
