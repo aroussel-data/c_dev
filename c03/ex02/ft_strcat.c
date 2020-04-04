1531b7720c80b5de85e9b40ce4f3dd0d8f8eb167
DDAaroussel <alex@deepdiveanalytics.com.au>
2020-04-04 17:47:23 +0200

    double checking against built strcat

--- c03/ex02/ft_strcat.c
+++ c03/ex02/ft_strcat.c
@@ -6,9 +6,11 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/18 14:23:11 by marvin            #+#    #+#             */
-/*   Updated: 2020/03/18 17:57:57 by marvin           ###   ########.fr       */
+/*   Updated: 2020/04/04 17:46:57 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_strlen(char *str)
 {
@@ -32,7 +34,7 @@ char	*ft_strcat(char *dest, char *src)
 
 	i = 0;
 	dest_length = ft_strlen(dest);
-
+	
 	while(src[i] != '\0')
 	{
 		dest[dest_length + i] = src[i];
@@ -40,4 +42,12 @@ char	*ft_strcat(char *dest, char *src)
 	}
 	dest[dest_length + i] = '\0';
        return dest;	
+}
+
+int	main(void)
+{
+	char a[] = "";
+	char b[] = "def";
+
+	printf("%s\n", ft_strcat(a, b));
 }
